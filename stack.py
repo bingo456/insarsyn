4eda93016982dcfc2a16b061324977d427ed26b4
Gerald Baier <gerald.baier@tum.de>
2017-11-24 17:32:46 +0100

    added multivariate rayleigh distribution with proper normalization

--- stack.py
+++ stack.py
@@ -50,6 +50,31 @@ def __multivariate_normal(cov, size):
     return xy
 
 
+def multivariate_rayleigh(cov, size):
+    """ multivariate rayleigh distribution
+
+    :params cov: covariance matrix. Must be positive-definite
+    :params size: number of samples
+    """
+
+    xs = np.random.normal(size=cov.shape[0]*size).reshape((-1, size))
+    ys = np.random.normal(size=cov.shape[0]*size).reshape((-1, size))
+
+    rs = np.sqrt(xs**2 + ys**2)
+
+    # normalization to unit variance
+    rs *= np.sqrt(2/(4-np.pi))
+
+    # transformation to desired correlated variance
+    L = np.linalg.cholesky(cov)
+    rs = np.dot(L, rs).T
+
+    if size == 1:
+        rs = rs.flatten()
+
+    return rs
+
+
 def amp_phi_coh2cov(amp, phi, coh):
     """ creates a covariance matrix from given amplidute and phase vectors.
     Coherence is a quadratic matrix with matching dimensions.
